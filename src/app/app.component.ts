1d6ce44225f89a5fa7f0b2a33718373bfa26979f
Dennys Yulieth Osorio <dyulieth@gmail.com>
2021-07-06 18:10:58 -0500

    create project practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practica';
+  array = ['Dennys', 'Yulieth', 'Mauricio'];
+  products = [
+    {
+      id: '1',
+      image: 'assets/images/imagen1.jpg',
+      title: 'Mascarilla Aloe vera',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/imagen2.jpg',
+      title: 'Mascarilla Ice cream',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/imagen3.jpg',
+      title: 'Esencia de caracol',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/imagen4.jpg',
+      title: 'Brillo Ushas',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/imagen5.jpg',
+      title: 'Vitamina C para ojos',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/imagen6.jpg',
+      title: 'Jabon Facial',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+  addItem(){
+    this.array.push('nuevo item');
+  }
+
+  deleteItem(index: number){
+    this.array.splice(index);
+  }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    id: String;
+    title: String;
+    price: number;
+    description: string;
+    image: string;
+}
